15c60200fb1691d8bd1fe610da3892ec870a245b
Maksim Shaposhnikov <smallhamster@MacBook-Air-Maksim.local>
2017-11-22 01:41:10 +0500

    cookie for current picture stat e

--- squad/scripts/gallery-control.js
+++ squad/scripts/gallery-control.js
@@ -19,15 +19,18 @@ function Gallery() {
 
 }
 
+
+
 var g = new Gallery();
 var documentation = false;
 var view = false;
 var global_position = 0;
+init();
 
 function openWindow(event) {
   var pos = g.association[event.target.id];
   var imgObj = g.elements[pos];
-  var neighbours = g.getNeighbours(pos);
+  //var neighbours = g.getNeighbours(pos);
   
   global_position = pos;
   view = true;  
@@ -93,3 +96,52 @@ function nextPicture(direction) {
   	if (global_position >= g.elements.length)
   		global_position = 0;
 }
+
+function createCookie(name, value, daysToExpire=3, path="/") {
+	// var d = new Date();
+	// d.setTime(d.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+
+	// var expires = 'expires=' + d.toUTCString();
+	// + expires + ";" + "path=" + path;
+	document.cookie = name + "=" + value + ";"; 
+}
+
+
+function retrieveCoookie(name) {
+	var cookies = decodeURIComponent(document.cookie);
+	console.log(cookies);
+	var regExp = new RegExp('index'+'=([0-9]+);?(expires=([a-zA-Z0-9;,: ]+);?)?(path=([a-zA-Z0-9\/]+))?');
+	var match = cookies.match(regExp);
+	if (match == null)
+		return null;
+	return {'value' : match[1], 'expires' : match[2], 'path' : match[3]};
+}
+
+function deleteCookie(name) {
+	document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+}
+
+
+function init() {
+	var photoCache = retrieveCoookie('index');
+	if (photoCache != null) {
+		global_position = parseInt(photoCache.value);
+		view = true;
+		var imgObj = g.elements[global_position];
+  		document.getElementById('window').style.display = "block";
+  		var wide = document.getElementById('wide-image');
+  		wide.src = imgObj.src;
+  		// needs image loading effect
+	}
+}
+
+window.onbeforeunload = function reloading() {
+	if (view) {
+		createCookie('index', global_position);
+	}
+	else
+		deleteCookie('index');
+}
+
+
+
