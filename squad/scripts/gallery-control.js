38f49b04100ac3e266cd7e16e73bb8ecb345bec2
Maksim Shaposhnikov <smallhamster@MacBook-Air-Maksim.local>
2017-11-25 04:23:57 +0500

    animation in modal + thumbnails

--- squad/scripts/gallery-control.js
+++ squad/scripts/gallery-control.js
@@ -17,18 +17,23 @@ function Gallery() {
 	this.elements = document.getElementsByClassName('hover-shadow');
 
 	this.association = {};
+	this.paths = [];
+	this.max_views = [];
+
 	for (var i=0; i < this.elements.length; i++) {
 		this.association[this.elements[i].id] = i;
+		this.paths[i] = this.elements[i].getAttribute('data-src').replace(/thmb_/gi, '');
+		this.max_views[i] = null;
 	}
 
-	this.getNeighbours = function(pos){
+	this.getNeighboursIdx = function(pos){
 		if (pos < 0 || pos >= this.elements.length)
 			throw 'Troubleshooting case';
 		if (pos == 0)
-			return [this.elements[this.elements.length- 1], this.elements[1]]
+			return [this.elements.length- 1, 1]
 		if (pos == this.elements.length - 1)
-			return [this.elements[pos - 1], this.elements[0]]
-		return [this.elements[pos -1], this.elements[pos + 1]]
+			return [pos - 1, 0]
+		return [pos -1, pos + 1]
 	}
 
 
@@ -41,18 +46,18 @@ var documentation = false;
 var view = false;
 var global_position = 0;
 init();
-
+var d;
 
 function openWindow(event) {
-  var pos = g.association[event.target.id];
-  var imgObj = g.elements[pos];
+  var id = event.target.id;
+
+  var pos = g.association[id];
+  
   
   global_position = pos;
-  view = true;  
-  document.getElementById('window').style.display = "block";
+  view = true;
 
-  var wide = document.getElementById('wide-image');
-  wide.src = imgObj.src;
+  loadHighQuality(pos);
   initHash(global_position);
 
 }
@@ -89,7 +94,10 @@ function eventHandler(event) {
 }
 
 function closeWindow() {
-  document.getElementById('window').style.display = "none";
+  	var wide = document.getElementById('wide-image');
+	wide.style.display = 'none';
+  	document.getElementById('window').style.display = "none";
+  	document.getElementById('ld').style.display = 'block';
   
 }
 
@@ -101,12 +109,10 @@ function actOnDocumentation(state) {
 }
 
 function nextPicture(direction) {
-	var neighbours = g.getNeighbours(global_position);
-	var imgObj = direction > 0 ? neighbours[1] : neighbours[0];
-
-
-	var wide = document.getElementById('wide-image');
-  	wide.src = imgObj.src;
+	var neighboursIdx = g.getNeighboursIdx(global_position);
+	var position = direction > 0 ? neighboursIdx[1] : neighboursIdx[0];
+	
+	loadHighQuality(position);
 
   	global_position += direction;
   	if (global_position < 0)
@@ -124,10 +130,7 @@ function init() {
 	if (photoCache != null) {
 		global_position = parseInt(photoCache.value);
 		view = true;
-		var imgObj = g.elements[global_position];
-  		document.getElementById('window').style.display = "block";
-  		var wide = document.getElementById('wide-image');
-  		wide.src = imgObj.src;
+		loadHighQuality(global_position);
   		// needs image loading effect
 	}
 }
@@ -147,7 +150,7 @@ function btnclick() {
 	if (retrieveCoookie('theme') != null)
 		deleteCookie('theme');
 
-	createCookie('theme', g.elements[global_position].src);
+	createCookie('theme', g.max_views[global_position].src);
 
 }
 
@@ -157,11 +160,38 @@ window.onpopstate = function(event) {
 		var pos = parseInt(state.substring(1));
 		global_position = pos;
 		view = true;
-		var imgObj = g.elements[global_position];
-  		document.getElementById('window').style.display = "block";
-  		var wide = document.getElementById('wide-image');
-  		wide.src = imgObj.src;
-
+		loadHighQuality(global_position);
 	}
+}
+
+
+function loadHighQuality(idx) {
+  var path = g.paths[idx];
+
+  document.getElementById('window').style.display = "block";
+  document.getElementById('ld').style.display = 'block';
+  var wide = document.getElementById('wide-image');
+  wide.style.display = 'none';
+
+  var button = document.getElementById('th-btn');
+  button.style.visibility = 'hidden';
+
+  if (g.max_views[idx] == null) {
+  		g.max_views[idx] = new Image();
+  		g.max_views[idx].src = path;
+  }
+  var load_int = setInterval(
+  	function() {
+  		if (g.max_views[idx].complete == true) {
+  			var wide = document.getElementById('wide-image');
+  			wide.src = g.max_views[idx].src;
+  			document.getElementById('ld').style.display = 'none';
+  			wide = document.getElementById('wide-image').style.display = 'block';
+  			button.style.visibility = 'visible';
+  			clearInterval(load_int);
+  		}
+  	}, 500
+  	)
+
 }
 
